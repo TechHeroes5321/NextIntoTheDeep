3f21269be5e47bc45d5a698d1efa6db8ed844219
AceTheAro <33375332+AceTheAro@users.noreply.github.com>
2025-01-09 21:14:15 -0500

    First commit -Ace

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/constants/LConstants.java
@@ -17,6 +17,7 @@ public class LConstants {
         DriveEncoderConstants.leftRearEncoderDirection = Encoder.REVERSE;
         DriveEncoderConstants.rightRearEncoderDirection = Encoder.FORWARD;
     }
+
 }
 
 
